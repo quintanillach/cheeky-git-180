a390ef470f9e09038c2c853cc03f472cc79f9340
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-03-27 09:34:25 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeapYearChecker
+{
+    class leapYear
+    {
+        static void Main(string[] args)
+        {
+            //Constructor instantiates the object 
+            leapYear obj = new leapYear();
+            obj.readData();
+            obj.leap();
+        }
+        int y;
+        public void readData()
+        {
+            // User inputs the year they want to check
+            Console.WriteLine("Enter the year (YYYY) : ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public void leap()
+        {
+            //if, else statement to state whether year is leap or not leap
+            //if statement to check for remainders of 0 when year is divided by 4, 100, 400
+            //if conditions are met then the year is a leap year
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", y);
+            }
+            //When conditions in the if statement are false, execute the else statement 
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", y);
+            }
+            //Force user input to end program
+            Console.ReadLine();
+        }
+    }
+}
