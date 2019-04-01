21004c3fdbdeef74a92ae3329452427e37200ab8
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-01 12:11:31 -0700

    Added comments and labels (needs another look)
    Fixed source information; added initial comments and labels

--- (03) DisplayAbbreviation.cs
+++ (03) DisplayAbbreviation.cs
@@ -1,65 +1,56 @@
-﻿using System;
+/* Purpose: C# program to display the abbreviation of a text
+ * Source: Sanfoundry
+ * Source Author: Manish Bhojasia
+ * Source URL: https://www.sanfoundry.com/csharp-program-display-abbreviation-text/
+ * Date Accessed: 03/31/2019
+ */
+
+using System;                                                           //Imports System namespace
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DisplayAbbreviation
+namespace DisplayAbbreviation                                           //Establishes DisplayAbbreviation namespace
 {
-    //Function: C# program to display the abbreviation of a text
     class abbreviation
     {
-        //Declares the field (variable) 'str' outside of methods
-        //By declaring outside of a specific method
-        //We enable all methods to use the same field (variable) without having to repeat the code in each method
-        string str;
+        string str;                                                     //Declares the 'str' field to enable all methods to use the field and prevent redundancy
 
-        //Next, we create a the method 'readdata'
-        //readdata takes the user input and assigns the value to 'str'
-        public void Readdata()
+        public void ReadData()                                          //Create 'ReadData()' method
         {
-            //Ask for user to input a string of text to abbreviate
-            Console.WriteLine("Enter a String : ");
-
-            //Assign value to 'str' based on user input
-            str = Console.In.ReadLine();
+            Console.WriteLine("Enter a String : ");                     //Display request for user input for a string of text to abbreviate
+            str = Console.In.ReadLine();                                //Assign value to 'str' field based on user input
         }
 
-        public void abbre()
+        public void abbre()                                             //Create the 'abbre()' method
         {
-            char[] c, result;
-            int j = 0;
-            c = new char[str.Length];
-            result = new char[str.Length];
-            c = str.ToCharArray();
-            result[j++] = (char)((int)c[0] ^ 32);
-            result[j++] = '.';
+            char[] c, result;                                           //Declare the char and result arrays on the same line
+            int j = 0;                                                  //Declare int j and assign an initial value of 0
+            c = new char[str.Length];                                   //Instantiate new char array and use built-in property to retreive total number of elements from the string 'str'
+            result = new char[str.Length];                              //Instantiate new char array and use built-in property to retreive total number of elements from the string 'str'
+            c = str.ToCharArray();                                      //Uses built-in method to copy characters in this instance of the string 'str' to an array of Unicode characters
+            result[j++] = (char)((int)c[0] ^ 32);                       //Adds character to result[] 
+            result[j++] = '.';                                          //Appends 
 
-            for (int i = 0; i < str.Length -1; i++)
+            for (int i = 0; i < str.Length -1; i++)                     //
             {
-                if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n')
+                if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n')        //if the character in c[] is a space, tab, or break
                 {
-                    int k = (int)c[i + 1] ^ 32;
-                    result[j++] = (char)k;
-                    result[j++] = '.';
+                    int k = (int)c[i + 1] ^ 32;                         //Declare int 'k' and set value to int c[+1]
+                    result[j++] = (char)k;                              //Add character as element in result[]
+                    result[j++] = '.';                                  //Add '.' between characters
                 }
             }
-            Console.Write("The Abbreviation for {0} is ", str);
-            Console.WriteLine(result);
-            Console.ReadLine();
+            Console.Write("The Abbreviation for {0} is ", str);         //Display the initial string from user input
+            Console.WriteLine(result);                                  //Display the result[]
+            Console.ReadLine();                                         //Wait for the user to press a button before closing
         }
 
-        public static void Main()
+        public static void Main()                                       //Main method
         {
-            //Here we create a static constructor to instantiate an object from our class
-            abbreviation obj = new abbreviation();
-
-            //Using the instance of 'obj' we just created
-            //We invoke our methods above to save data to the object
-            obj.Readdata();
-            obj.abbre();
+            abbreviation obj = new abbreviation();                      //Create a static constructor to instantiate an object from our class
+            obj.ReadData();                                             //Invoke ReadData Method
+            obj.abbre();                                                //Invoke abbre Method
         }
     }
-    //Source: Sanfoundry
-    //URL: https://www.sanfoundry.com/csharp-program-display-abbreviation-text/
-    //Date Accessed: 03/31/2019
 }
