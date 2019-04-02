395efe36c8de107a30d7dc75741a23d0bab99af7
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-02 09:26:26 -0700

    Removed extraneous spaces

--- (01) LeapYearChecker.cs
+++ (01) LeapYearChecker.cs
@@ -10,33 +10,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LeapYearChecker                                                                                   //Namespace
+namespace LeapYearChecker                                               //Namespace
 {
-    class leapYear                                                                                          //Class
+    class leapYear                                                      //Class
     {
-        static void Main(string[] args)                                                                     //Main method
+        static void Main(string[] args)                                 //Main method
         {
-            leapYear obj = new leapYear();                                                                  //Constructor
-            obj.readData();                                                                                 //Method Call readData()
-            obj.leap();                                                                                     //Method Call leap()
+            leapYear obj = new leapYear();                              //Constructor
+            obj.readData();                                             //Method Call readData()
+            obj.leap();                                                 //Method Call leap()
         }
-        int y;                                                                                              //Declare field 'y'
-        public void readData()                                                                              //Method readData()
+        int y;                                                          //Declare field 'y'
+        public void readData()                                          //Method readData()
         {
-            Console.WriteLine("Enter the year (YYYY) : ");                                                  //Display request for user input to console
-            y = Convert.ToInt32(Console.ReadLine());                                                        //Save user input to 'y'
+            Console.WriteLine("Enter the year (YYYY) : ");              //Display request for user input to console
+            y = Convert.ToInt32(Console.ReadLine());                    //Save user input to 'y'
         }
-        public void leap()                                                                                  //Method leap()
-        {                                                                                                   //if, else statement to state whether year is leap or not leap
-            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))                                             //if statement to check for remainders of 0 when year is divided by 4, 100, 400
+        public void leap()                                              //Method leap()
+        {                                                               //if, else statement to state whether year is leap or not leap
+            if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))         //if statement to check for remainders of 0 when year is divided by 4, 100, 400
             {
-                Console.WriteLine("{0} is a Leap Year", y);                                                 //when conditions are true then the year is a leap year
+                Console.WriteLine("{0} is a Leap Year", y);             //when conditions are true then the year is a leap year
             }
-            else                                                                                            //else statement executes in all other cases
+            else                                                        //else statement executes in all other cases
             {
-                Console.WriteLine("{0} is not a Leap Year", y);                                             //When conditions are false, the year is not a leap year 
+                Console.WriteLine("{0} is not a Leap Year", y);         //When conditions are false, the year is not a leap year 
             }
-            Console.ReadLine();                                                                             //Force user input to end program
+            Console.ReadLine();                                         //Force user input to end program
         }
     }
 }
