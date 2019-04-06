4d7f534d5b08ac0d450b8b6fb0b5b65e0e5450ba
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-06 15:46:27 -0700

    Update Exceptions_IndexOutOfRange.cs
    Added and finalized comments based on knowledge of exception handling

--- Conceptual/Exceptions_IndexOutOfRange.cs
+++ Conceptual/Exceptions_IndexOutOfRange.cs
@@ -9,21 +9,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-namespace differnce
+
+namespace Difference
 {
-    class arrayoutofindex
+    // Renamed the classes and methods using Pascal case per .NET conventions
+    class ArrayOutOfIndex
     {
-        public void calculatedifference()
+        public void CalculatedDifference()
         {
-            int difference=0;
-            int [] number= new int[5] {1,2,3,4,5};
+            int difference = 0;
+            // This program initializes an array of integers using the new keyword
+            // but in the original program, the length specified was used inside the
+            // initialization. This is unnecessary since the array elements are already
+            // declared on the same line and no new elements are added later.
+            int [] number= new int[] {1,2,3,4,5};
+
+            // This program uses the try-catch statement to handle exceptions and prevent
+            // the program from crashing. The try block guards the code which may cause
+            // the errors. In this case, the array 'number' only contains five elements
+            // which makes it very likely that an user would input a value outside of
+            // its bounds. The code inside of the try block for this program checks if
+            // the initial number is within the scope of the array and prints the result
+            // when possible. The catch clause underneath then handles values outside of
+            // the range of the array by displaying the message based on the exception
+            // parameters.
             try
             {
-                for (int init =1; init <=5; init++)
+                for (int init = 1; init <= 5; init++)
                 {
-                    difference= difference -  number[init];
+                    difference = difference - number[init];
                 }
-                Console.WriteLine("The difference of the array is:" + difference);
+                // The original program used the '+' operator to concatenate the
+                // string and the 'difference' variable. I decided to use the '$'
+                // character to take advantage of the string interpolation and improve
+                // readability. The function still performs the same action as before.
+                Console.WriteLine($"The difference of the array is: {difference}");
             }
             catch (IndexOutOfRangeException e)
             {
@@ -31,13 +51,16 @@ namespace differnce
             }
         }
     }
-    class classmain
+    class ClassMain
     {
         static void Main(string [] args)
-    {
-        arrayoutofindex obj = new arrayoutofindex();
-        obj.calculatedifference();
+        {
+        // This constructor instantiates the ArrayOutOfIndex object and passes
+        // the object to the CalculatedDifference Method. The program then 
+        // waits for the user to press Enter before terminating.
+        ArrayOutOfIndex obj = new ArrayOutOfIndex();
+        obj.CalculatedDifference();
         Console.ReadLine();
-    }
+        }
 }
 }
