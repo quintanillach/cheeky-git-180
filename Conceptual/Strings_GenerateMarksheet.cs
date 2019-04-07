059ad8d56c741401525ec01bc1af86f4dd3bf1e5
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-06 20:54:06 -0700

    Update Strings_GenerateMarksheet.cs
    Reformatted comments to new format and finalized explanation of functions. May revisit for refactoring into multiple methods

--- Conceptual/Strings_GenerateMarksheet.cs
+++ Conceptual/Strings_GenerateMarksheet.cs
@@ -8,56 +8,78 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;                                                  //Imports System namespace
+using System.Text;
 
-namespace GenerateMarksheet                                         //Create GenerateMarksheet namespace
+
+//Renamed namespace to conform to my naming convention for Github repo
+namespace Strings
 {
-    class Program
+    // Renamed program from Program to GenerateMarksheet
+    class GenerateMarksheet
     {
-        static void Main(string[] args)                             //Main method
+        static void Main(string[] args)
         {
-            int r, m1, m2, m3, t;                                   //Declare multiples integer variables
-            float p;                                                //Declare float variable
-            string n;                                               //Declare string n
+            // The program immediately declares multiple variables based on the
+            // predicted number of subjects and then requests the user to input
+            // values to assign to each of these variables.
+            int r, m1, m2, m3, t;
+            float p;
+            string n;
 
-            Console.WriteLine("Enter Roll Number : ");              //Display request for student Roll number
-            r = Convert.ToInt32(Console.ReadLine());                //Convert user input to integer and save to int 'r'
+            Console.WriteLine("Enter Roll Number : ");
+            r = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Student Name : ");             //Display request for student name
-            n = Console.ReadLine();                                 //Save user input to string 'n'
+            Console.WriteLine("Enter Student Name : ");
+            n = Console.ReadLine();
 
-            Console.WriteLine("Mark of Subject1 : ");               //Display request for first subject mark
-            m1 = Convert.ToInt32(Console.ReadLine());               //Convert user input to integer and save to int 'm1'
+            Console.WriteLine("Mark of Subject1 : ");
+            m1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Mark of Subject2 : ");               //Display request for second subject mark
-            m2 = Convert.ToInt32(Console.ReadLine());               //Convert user input to integer and save to int 'm2'
+            Console.WriteLine("Mark of Subject2 : ");
+            m2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(" Mark of Subject3 : ");              //Display request for third subject mark
-            m3 = Convert.ToInt32(Console.ReadLine());               //Convert user input to integer and save to int 'm3'
+            Console.WriteLine(" Mark of Subject3 : ");
+            m3 = Convert.ToInt32(Console.ReadLine());
 
-            t = m1 + m2 + m3;                                       //Sum the three marks and save to int 't'
-            p = t / 3.0f;                                           //Average the three marks and implicit conversion to float p for precision
-            Console.WriteLine("Total : ", +t);                      //Display int 't' as the total
-            Console.WriteLine("Percentage : ", +p);                 //Display float 'p' as the percentage
+            // Based on the user input, the variables are then summed up
+            // to int 't' and averaged out for a percentage. The total is
+            // converted from a integer to a float for precision since integers
+            // do not necessarily divide into round integers. The program
+            // prints these values before moving on the next segment.
+            t = m1 + m2 + m3;
+            p = t / 3.0f;
+            Console.WriteLine("Total : ", +t);
+            Console.WriteLine("Percentage : ", +p);
 
+            // Changed the if functions to else if functions for readability
+            // Changed the values in the if statement to correspond with logical
+            // letter grade-percentage values
+            // This if else statement evaluates the average percentage of the
+            // marks given above and assigns a letter grade to it. The statement
+            // executes sequentially so that if the 
             if (p >= 35 && p < 50)                                  //if statements to determine grades based on percentages 
             {
                 Console.WriteLine("Grade is C");                    //Display Grade is C for 35 to 49 percent
             }
 
-            if (p >= 50 && p < 60)
+            else if (p >= 50 && p < 60)
             {
                 Console.WriteLine("Grade is B");                    //Display Grade is B for 50 to 59 percent
             }
 
-            if (p >= 60 && p < 80)
+            else if (p >= 60 && p < 80)
             {
                 Console.WriteLine("Grade is A");                    //Display Grade is A for 60 to 79 percent
             }
 
-            if (p >= 80 && p <= 100)
+            else if (p >= 90 && p <= 100)
             {
                 Console.WriteLine("Grade is A+");                   //Display Grade is A+ for 80 to 100 percent
+            }
+
+            else
+            {
+                Console.WriteLine("The percentage is not worthy of a letter grade.");
             }
             Console.ReadLine();                                     //Wait for user to close program
         }
