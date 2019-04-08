2d72eef0b11f2822d51a461f5138db7cd7dc008d
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-08 12:44:27 -0700

    Fixed line spacing for readability

--- Conceptual/Arrays_CopyBetweenArrays(Edited).cs
+++ Conceptual/Arrays_CopyBetweenArrays(Edited).cs
@@ -11,33 +11,34 @@ using System;
 namespace Arrays
 {
  class CopyBetweenArrays
-{
-    static void Main()
+ {
+   static void Main()
+   {
+    // These integer variables are declared at the beginning of the program
+    //
+    int n, m, size;
+    Console.WriteLine("Enter the size of the Array : ");
+    n = Convert.ToInt32(Console.ReadLine());
+    int [] a = new int[n];
+    Console.WriteLine("Enter the Elements of the First Array :");
+    for (int i = 0; i <n; i++)
+      {
+        a[i] = Convert.ToInt32(Console.ReadLine());
+      }
+    Console.WriteLine("Enter the Size of the Target Array : ");
+    m = Convert.ToInt32(Console.ReadLine());
+    int[] target = new int[m];
+    Console.WriteLine("Enter the section of the First Array "+ 
+                      "that has to be Copied :");
+    size = Convert.ToInt32(Console.ReadLine());
+    Array.Copy(a, 0, target, 0, size);
+    Console.WriteLine("New Array With The Specified Section of Elements "+
+                      "in the First Array");
+    foreach (int value in target)
     {
-        int n, m, size;
-        Console.WriteLine("Enter the size of the Array : ");
-        n = Convert.ToInt32(Console.ReadLine());
-        int [] a = new int[n];
-        Console.WriteLine("Enter the Elements of the First Array :");
-        for (int i = 0; i <n; i++)
-        {
-            a[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        Console.WriteLine("Enter the Size of the Target Array : ");
-        m = Convert.ToInt32(Console.ReadLine());
-        int[] target = new int[m];
-        Console.WriteLine("Enter the section of the First Array "+ 
-                          "that has to be Copied :");
-        size = Convert.ToInt32(Console.ReadLine());
-        Array.Copy(a, 0, target, 0, size);
-        Console.WriteLine("New Array With The Specified Section of Elements "+
-                          "in the First Array");
-        foreach (int value in target)
-        {
-            Console.WriteLine(value);
-        }
-        Console.Read();
+       Console.WriteLine(value);
     }
-}
-
+    Console.Read();
+    }
+  }
 }
