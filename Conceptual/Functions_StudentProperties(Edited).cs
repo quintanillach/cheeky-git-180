be908ffcf97826f52202636fb34228555d206d5a
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-08 14:48:33 -0700

    Finalized comments and Refactored Main()
    Finalized the comments on the program and refactor the Main() method

--- Conceptual/Functions_StudentProperties(Edited).cs
+++ Conceptual/Functions_StudentProperties(Edited).cs
@@ -11,7 +11,12 @@ namespace Functions
 {
     class Student
     {
-        
+        // The two fields 'myName' and 'myAge' are declared with the private
+        // access modifier and implemented as backing fields rather than public
+        // to guard the program from unexpected input. The properties 'Name' and
+        // 'Age' are then declared with the get and set accessors which allows
+        // the program expose only the desired information without revealing
+        // private data or risking corruption.
         private string myName = "N/A";
         private int myAge = 0;
         public string Name
@@ -25,6 +30,9 @@ namespace Functions
             set { myAge = value; }
         }
 
+        // The override modifier here allows the program to extend or modify the
+        // abstract value of the method based on the inherited properties from
+        // above.
         public override string ToString()
         {
             return "Name = " + Name + ", Age = " + Age;
@@ -32,13 +40,19 @@ namespace Functions
 
         public static void Main()
         {
+            // This program uses a default constructor to instantiate an object
+            // of the same name as the class -- in this case 'Student'. The program
+            // then prints the details of the student. The original program did not allow
+            // the user to input any value and instead just printed prefabricated information.
+            // I refactored this segment to receive user input and save the information
+            // to the Student properties. I also used string interpolation over composite
+            // formatting in the Console.WriteLine functions
             Student Student = new Student();
-            Console.WriteLine("Student details - {0}", Student);
-            Student.Name = "BOB";
-            Student.Age = 99;
-            Console.WriteLine("Student details - {0}", Student);
-            Student.Age += 1;
-            Console.WriteLine("Student details - {0}", Student);
+            Console.WriteLine("Enter Student Name : ");
+            Student.Name = (Console.ReadLine());
+            Console.WriteLine("Enter Student Age : ");
+            Student.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Student details - {Student}");
             Console.ReadLine();
         }
     }
