e2109a53c05d447e64a851275fdad7fd7f559723
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-08 15:22:48 -0700

    Finalized comments and refactoring

--- Conceptual/Recursions_FibonacciSequence(Edited).cs
+++ Conceptual/Recursions_FibonacciSequence(Edited).cs
@@ -15,11 +15,20 @@ namespace Recursions
 	    	{
 			int p = 0;
 			int q = 1;
+			// This for loop iterates the integer variables above and
+			// assigns the value 'p' to a temporary variable, assigns the value of q
+			// to the 'p' variable, and then adds q and and the previous value of p
+			// which is now the value of 'temp' and assigns the sum back to 'q'.
+			// The loop iterates as many times as the user requested until it i = n
+			// and the loop is broken. Through each iteration, the method prints the
+			// value of p and the recursion continues.
+			// Refactored the for loop with the addition assignment (+=) operator
+			// for concision
 			for (int i = 0; i < n; i++)
 			{
-		    	int temp = p;
-		    	p = q;
-		    	q = temp + q;
+		    		int temp = p;
+		    		p = q;
+		    		q += temp;
 			}
 			return p;
 	    	}
@@ -29,12 +38,15 @@ namespace Recursions
 			Console.WriteLine("\n\n Recursion : Find the Fibonacci numbers for a n numbers of series :");
 			Console.WriteLine("-----------------------------------------------------------------------");
 
+			// This Main() method calls the FindFibonacci() method and passes the returned value for as
+			// many times as user indicates.
+			// Refactored this segment using string interpolation for readability without sacrificing function
 			Console.Write(" Input number of terms for the Fibonacci series : ");
 			int n1 = Convert.ToInt32(Console.ReadLine());
-			Console.Write("\n The Fibonacci series of {0} terms is : ",n1);
+			Console.Write($"\n The Fibonacci series of {n1} terms is : ");
 		    	for (int i = 0; i < n1; i++)
 			{
-			    Console.Write("{0} ",FindFibonacci(i));
+			    Console.Write($"{FindFibonacci(i)} ");
 			}
 			Console.ReadKey();
 		}
