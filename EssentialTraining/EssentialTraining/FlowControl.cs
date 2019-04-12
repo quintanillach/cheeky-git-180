b9c1e8540553d52ddd1c4ad2c951c8ea6843607d
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-11 21:47:42 -0700

    Finished the EssentialTrainingApp

---
+++ EssentialTraining/EssentialTraining/FlowControl.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public string IsYourNameNotBruce(string yourName)
+        {
+            if (yourName.ToLower() != "bruce")
+            {
+                return "Can we call you Bruce for the sake of simplicity?";
+            }
+            else
+            {
+                return "G'day Bruce";
+            }
+        }
+
+        public string GradeLetter (int score)
+        {
+            if (score > 90)
+            {
+                return "A";
+            }
+            else if (score > 79 && score < 90)
+            {
+                return "B";
+            }
+            else if (score >= 70 && score < 80)
+            {
+                return "C";
+            }
+            else if (score >= 60 && score < 70)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+        public string PrimaryOrSecondaryCompound (string color)
+        {
+            if (color.ToLower() == "red" || color.ToLower() == "blue" || color.ToLower() == "yellow")
+            {
+                return "Primary";
+            }
+            else
+            {
+                return "Secondary";
+            }
+        }
+        //public string SecondaryOrPrimary (string color)
+        //{
+        //    var result = "";
+        //    switch (color.ToLower())
+        //    {
+        //        case "red":
+        //            result = "Primary";
+        //            break;
+        //        case "blue":
+        //            result = "Primary";
+        //            break;
+        //        case "yellow":
+        //            result = "Primary";
+        //            break;
+        //        default:
+        //            result = "Secondary";
+        //            break;
+        //    }
+        //}
+        //public string PrimaryOrSecondary(string color)
+        //{
+        //    var result = "";
+        //    if (color.ToLower() == "red")
+        //    {
+        //        result = "Primary";
+        //    }
+        //    else if (color.ToLower() == "blue")
+        //    {
+        //        result = "Primary";
+        //    }
+        //    else if (color.ToLower() == "yellow")
+        //    {
+        //        result = "Primary";
+        //    }
+        //    else
+        //    {
+        //        result = "Secondary";
+        //    }
+        //}
+        public bool IsYourFavoriteColorYellow (string color)
+            {
+            return (color.ToLower() == "yellow");
+            }
+        public bool IsYourFavoriteColorGreen (string color)
+        {
+            return (color.ToLower() == "green") ? true : false;
+        }
+        public bool IsYourFavoriteColorRed (string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;
+        }
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            if (color.ToLower() == "blue")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EssentialTraining/EssentialTrainingApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NLog;
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            logger.Trace("The program started");
+            Words = new List<string>();
+            Words.Add("Bread");
+            Words.Add("Milk");
+            Words.Add("Cheese");
+
+            CrazyMathProblem();
+            ReadTextFile();
+            Console.ReadLine();
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+            {
+                using (var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+
+            catch (System.IO.DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the directory" + ex.Message);
+                logger.Error("The directory was not found" + ex.Message);
+            }
+
+            catch(System.IO.FileNotFoundException ex)
+            {
+                Console.WriteLine("Could not find the File" + ex.Message);
+                logger.Error(ex.Message);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("Unknown error occurred" + ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("The finally runs all the time");
+            }
+        }
+        private static int CrazyMathProblem()
+        {
+            var income = 1000;
+            for (var i = 10; i > 0; i--)
+            {
+                income = income = (income / i);
+            }
+
+            return income;
+        }
+    }
+}
