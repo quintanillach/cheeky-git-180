ac025d388225b57604c629a0ddaa52d932ecd21b
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-14 10:49:40 -0700

    Update LCMandGCD.cs
    Added namespace and renamed classes and methods using Pascal case

--- Conceptual/Recursions/LCMandGCD.cs
+++ Conceptual/Recursions/LCMandGCD.cs
@@ -8,38 +8,41 @@
 
 using System;
 using System.Text;
- 
-class RecExercise12
-{        
-  public static void Main()
+
+namespace Recursions
+{
+    class LCMandGCD
     {
+        public static void Main()
+        {
             long num1, num2, hcf, lcm;
             Console.WriteLine("\n\n Recursion : Find the LCM and GCD of two numbers :");
-		    Console.WriteLine("------------------------------------------------------");      
-      
-            Console.Write(" Input the first number : "); 
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.Write(" Input the first number : ");
             num1 = Convert.ToInt64(Console.ReadLine());
-            Console.Write(" Input the second number : "); 
+            Console.Write(" Input the second number : ");
             num2 = Convert.ToInt64(Console.ReadLine());
- 
+
             hcf = gcd(num1, num2);
             lcm = (num1 * num2) / hcf;
- 
+
             Console.WriteLine("\n The GCD of {0} and {1} = {2} ", num1, num2, hcf);
             Console.WriteLine(" The LCM of {0} and {1} = {2}\n", num1, num2, lcm);
-            
- 
-     }
- 
-       static long gcd(long n1, long n2)
-       {
-           if (n2 == 0)
-           {
-               return n1;
-           }
-           else
-           {
-               return gcd(n2, n1 % n2);
-           }
-       }
+
+
+        }
+
+        static long GCD(long n1, long n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return gcd(n2, n1 % n2);
+            }
+        }
+    }
 }
