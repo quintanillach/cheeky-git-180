dca58f281b5fd4e86a77db0fb6963613a8e453d9
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-04-19 12:54:50 -0700

    Refactored and marked up Interfrace and Bicycle class
    Added public access modifiers to appropriate members; commented on interface and Bicycle class; implemented string interpolation and expression bodied members; Renamed methods using Pascal case

--- Conceptual/Interfaces/ImplementInterfaces(Original).cs
+++ Conceptual/Interfaces/ImplementInterfaces(Original).cs
@@ -12,45 +12,52 @@
  {
     public interface Vehicle
     { 
-        // These are abstract methods which do not have any 
-        public abstract void changeGear(int a); 
-        public abstract void speedUp(int a); 
-        public abstract void applyBrakes(int a); 
-    } 
+        // These are abstract methods which do not
+        // method bodies and forces the derived class
+        // to override the abstract methods (known as dynamic polymorphism)
+        // Added the public access modifier to give access to the interface
+        // outside of the current assembly
+        public abstract void ChangeGear(int a); 
+        public abstract void SpeedUp(int a); 
+        public abstract void ApplyBrakes(int a); 
+    }
 
-      // class implements interface 
-      class Bicycle : Vehicle{ 
-
+    // The Bicycle class inherits from the Vehicle interface
+    // The derived class must contain the methods inside the
+    // interface to "fulfill the contract"
+    // Added the public access modifier for potential testing
+    // Renamed methods using Pascal case
+    public class Bicycle : Vehicle
+    { 
         int speed; 
         int gear; 
 
-        // to change gear 
-        public void changeGear(int newGear) 
+        // Each of these methods override the abstract methods
+        // by declaring a parameter variable and assigning value
+        // to the local variables based on paramters passed from
+        // the Main() method
+        public void ChangeGear(int newGear) 
         { 
-
           gear = newGear; 
         } 
 
-        // to increase speed 
-        public void speedUp(int increment) 
+        public void SpeedUp(int increment) 
         { 
 
           speed = speed + increment; 
         } 
 
-        // to decrease speed 
-        public void applyBrakes(int decrement) 
+        public void ApplyBrakes(int decrement) 
         { 
 
           speed = speed - decrement; 
         } 
 
-        public void printStates() 
-        { 
-          Console.WriteLine("speed: " + speed + 
-                  " gear: " + gear); 
-        } 
-      } 
+        public void printStates() =>
+            // Used string interpolation for readability
+            // Implemented expression-bodied members for conciseness
+            Console.WriteLine($"speed: {speed} \n gear: {gear}"); 
+    }
 
       // class implements interface 
       class Bike : Vehicle { 
@@ -59,21 +66,21 @@
         int gear; 
 
         // to change gear 
-        public void changeGear(int newGear) 
+        public void ChangeGear(int newGear) 
         { 
 
           gear = newGear; 
         } 
 
         // to increase speed 
-        public void speedUp(int increment) 
+        public void SpeedUp(int increment) 
         { 
 
           speed = speed + increment; 
         } 
 
         // to decrease speed 
-        public void applyBrakes(int decrement){ 
+        public void ApplyBrakes(int decrement){ 
 
           speed = speed - decrement; 
         } 
@@ -95,18 +102,18 @@
             // creating an instance of Bicycle 
             // doing some operations 
             Bicycle bicycle = new Bicycle(); 
-            bicycle.changeGear(2); 
-            bicycle.speedUp(3); 
-            bicycle.applyBrakes(1); 
+            bicycle.ChangeGear(2); 
+            bicycle.SpeedUp(3); 
+            bicycle.ApplyBrakes(1); 
 
             Console.WriteLine("Bicycle present state :"); 
             bicycle.printStates(); 
 
             // creating instance of bike. 
             Bike bike = new Bike(); 
-            bike.changeGear(1); 
-            bike.speedUp(4); 
-            bike.applyBrakes(3); 
+            bike.ChangeGear(1); 
+            bike.SpeedUp(4); 
+            bike.ApplyBrakes(3); 
 
             Console.WriteLine("Bike present state :"); 
             bike.printStates(); 
