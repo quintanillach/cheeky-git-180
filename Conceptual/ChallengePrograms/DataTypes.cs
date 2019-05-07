94a1ab8ce94b7c9acc6ab5546fce671bbc8f096e
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-05-07 08:45:37 -0700

    Updated program with user input and safety

--- Conceptual/ChallengePrograms/DataTypes.cs
+++ Conceptual/ChallengePrograms/DataTypes.cs
@@ -9,29 +9,47 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
+// Added namespace
 namespace ChallengePrograms
 {
+	// Added public access modifiers throughout program
+	// for testing purposes
 	public class Solution 
 	{
 		public static void Main(String[] args) 
 		{
+			Console.WriteLine("--------------------------------------------");
+			Console.WriteLine("Challenge program to concatenate data types");
+			Console.WriteLine("--------------------------------------------\n");
+			
+			// These were the given input variables from HackerRank
 			int i = 4;
 			double d = 4.0;
 			string s = "HackerRank ";
-
-			int j = 0 ;
-			double e = 0.0;
-			string t = " ";
-
-			j = int.Parse(Console.ReadLine());
-			e = Convert.ToDouble(Console.ReadLine());
-			t = Console.ReadLine();
-
+			
+			// Declared variables for concatenation				
+			// User input as string and TryParse
+			// to convert string to numeric data types
+			Console.WriteLine("Enter a number (integer) : ");
+			string J = (Console.ReadLine());
+			int.TryParse(J, out int j);
+			
+			Console.WriteLine("Enter a number (double) : ");
+			string E = (Console.ReadLine());
+			double.TryParse(E, out double e);
+			
+			Console.WriteLine("Enter a string : ");
+			string t = Console.ReadLine();
+			
+			// Expressions to concatenate user input with
+			// given variables
 			int sumInts = i + j;
-			Console.WriteLine(sumInts);
+			Console.WriteLine($"\n{sumInts}");
 
 			double sumDoubles = d + e;
-			Console.WriteLine(sumDoubles.ToString("F1"));
+			// The result of the double concatenated
+			// and converted back to string and
+			Console.WriteLine($"{sumDoubles:F1}");
 
 			string sumStrings = s + t;
 			Console.WriteLine(sumStrings);
