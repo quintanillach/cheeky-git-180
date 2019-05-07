ca50d8714a60ffc2956c97e613ee765e1d8b353b
Chris Quintanilla <48834453+quintanillach@users.noreply.github.com>
2019-05-07 09:13:35 -0700

    Refactored for user input and better precision

--- Conceptual/ChallengePrograms/MealTotalCost.cs
+++ Conceptual/ChallengePrograms/MealTotalCost.cs
@@ -19,25 +19,52 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
+namespace ChallengePrograms
+{
+    public class MealTotalCost 
+    {
+        public static void Calculate(double meal_cost, int tip_percent, double tax_percent) 
+        {
+            // Parameters are saved to local variables
+            double m = meal_cost;
+            
+            // The tip and tax are calculated against the given
+            // meal cost
+            double p = (m * tip_percent) / 100;
+            double x = (m * tax_percent) / 100;
+            
+            // Local variables are summed up
+            // to return total meal cost
+            double total_cost = m + p + x;
+            
+            // Print the total cost to the appropriate
+            // numeric format of two decimal places 
+            Console.WriteLine($"\nTip - {p}\nTax - {x:F2}\nFinal Cost - {total_cost:F2}");
+        }
 
-    // Complete the solve function below.
-    static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        double m = meal_cost;
-        double p = (m * tip_percent) / 100;
-        double x = (m * tax_percent) / 100;
-        double total_cost = m + p + x;
-        int answer = (int) Math.Round(total_cost);
-        Console.WriteLine(answer);
-    }
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("Challenge program to calculate total meal cost including tax and tip");
+            Console.WriteLine("----------------------------------------------------------------------\n");
+            
+            // User input as string and converted to numeric
+            // data types for calculation using TryParse
+            Console.WriteLine("Enter the meal cost :");
+            string input1 = (Console.ReadLine());
+            double.TryParse(input1, out double meal_cost);
+            
+            Console.WriteLine("Enter the tip as a percentage of the meal cost :");
+            string input2 = (Console.ReadLine());
+            int.TryParse(input2, out int tip_percent);
 
-    static void Main(string[] args) {
-        double meal_cost = Convert.ToDouble(Console.ReadLine());
-
-        int tip_percent = Convert.ToInt32(Console.ReadLine());
-
-        int tax_percent = Convert.ToInt32(Console.ReadLine());
-
-        solve(meal_cost, tip_percent, tax_percent);
+            Console.WriteLine("Enter the local sales tax as a percentage:");
+            string input3 = (Console.ReadLine());
+            double.TryParse(input3, out double tax_percent);
+            
+            // The Calculate method is called and the user input
+            // is passed as arguments
+            Calculate(meal_cost, tip_percent, tax_percent);
+        }
     }
 }
